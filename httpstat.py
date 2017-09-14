27747059cf88008a9bca1f0888f85b75bef6906d
GanquanWen <31709654+GanquanWen@users.noreply.github.com>
2017-09-14 19:37:59 -0400

    Color code changes

--- httpstat.py
+++ httpstat.py
@@ -99,12 +99,12 @@ def make_color(code):
     return color_func
 
 
-red = make_color(31)
-green = make_color(32)
-yellow = make_color(33)
-blue = make_color(34)
-magenta = make_color(35)
-cyan = make_color(36)
+red = make_color(32)
+green = make_color(33)
+yellow = make_color(34)
+blue = make_color(35)
+magenta = make_color(36)
+cyan = make_color(361)
 
 bold = make_color(1)
 underline = make_color(4)
